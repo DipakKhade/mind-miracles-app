7d35133fd965d3ecbdbc95e0c99031c6c4d02413
Dipak Khade <dipakhade214@gmail.com>
2026-04-17 08:53:44 +0530

    exporting envs

---
+++ app.config.js
@@ -0,0 +1,8 @@
+export default {
+    expo: {
+      extra: {
+        googleWebClientId: process.env.GOOGLE_WEB_CLIENT_ID,
+        googleIosClientId: process.env.GOOGLE_IOS_CLIENT_ID,
+      }
+    }
+  }
