7a2e21da61ab5484d1471ce1cbdf1b9205bfd392
Dipak Khade <dipakhade214@gmail.com>
2026-04-19 10:37:04 +0530

    added package name

--- app.config.js
+++ app.config.js
@@ -1,5 +1,8 @@
 export default {
     expo: {
+      android: {
+        package: 'com.dipak8999.mindmiraclesapp',
+      },
       extra: {
         googleWebClientId: process.env.GOOGLE_WEB_CLIENT_ID,
         googleIosClientId: process.env.GOOGLE_IOS_CLIENT_ID,
